8afbeee3e4a08db2583b70f8a1b9466aa7c21ba0
shinkatayama <shinkatayama@shinkatayamanoMacBook-Pro-4.local>
2019-03-19 11:45:49 +0000

    remove config

--- config.py
+++
@@ -1,3 +0,0 @@
-g_project_id = 'xxxxxxxx' #Google Cloud Platform project ID 
-g_client_access_token = 'xxxxxxxx' #Google Cloud Platform Access Token
-tts_key = "xxxxxxxx" #IBM Watson Text To Speech Key
